a0c0b7928ef5bdfee56b3904522a5c49fedb3ced
joyblack <1016037677@qq.com>
2019-12-18 17:19:51 +0800

    changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="home">
+    <button @click="handleClick">返回上一页</button>
+    <button @click="jump">argu</button>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -13,6 +15,19 @@ export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+  methods: {
+    handleClick () {
+      // 获取路由实例
+      this.$router.go(-1)
+    },
+    jump () {
+      const name = 'zhaoyi'
+      this.$router.push({
+        path: `/argu/${name}`,
+        props: true
+      })
+    }
   }
 }
 </script>
